e06402ed13330d6dd51bcd1489f523830a727e3b
J4y0211 <131111969+J4y0211@users.noreply.github.com>
2023-11-14 10:21:46 -0400

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola soy jason</h1>
+    <p>Matricula: 2021-0238</p>
+    <p>Esta tarea es usando docker compose en una pagina con php y mysql</p>
+</body>
+</html>
